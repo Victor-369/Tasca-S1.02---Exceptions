86cbac2763f0bb922e748734596cdd68849c4aff
Victor Garcia <victor.garcia.florez@gmail.com>
2026-09-24 18:14:44 +0200

    fix: improve code

--- src/main/java/com/pruebas/proyecto/nivell1/exceptions/EmptySaleException.java
+++ src/main/java/com/pruebas/proyecto/nivell1/exceptions/EmptySaleException.java
@@ -1,6 +1,6 @@
 package com.pruebas.proyecto.nivell1.exceptions;
 
-public class EmptySaleException extends Exception {
+public class EmptySaleException extends RuntimeException {
     public EmptySaleException(String message) {
         super(message);
     }
